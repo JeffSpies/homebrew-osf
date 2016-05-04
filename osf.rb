066a968abfa19fc148858d9de170404fa9c35af6
Jeffrey Spies <jspies@gmail.com>
2016-05-03 23:45:57 -0400

    npm

--- osf.rb
+++ osf.rb
@@ -5,8 +5,12 @@ class Osf < Formula
     depends_on "python" => :recommended
     depends_on "tokutek/tokumx/tokumx-bin"
     depends_on "rabbitmq"
+    depends_on "libxml2"
+    depends_on "libxslt"
+    depends_on "node"
     
     def install
+        "#{HOMEBREW_PREFIX}/bin/npm" install -g bower
         system "echo hi"
     end
 end
