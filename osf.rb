a6e7f7a39515997ac20126449397cc1cb66252ea
Jeffrey Spies <jspies@gmail.com>
2016-05-03 23:32:43 -0400

    adding a few dependencies

--- osf.rb
+++ osf.rb
@@ -2,6 +2,10 @@ class Osf < Formula
     homepage "http://osf.io"
     head "https://github.com/centerforopenscience/osf.io.git"
     
+    depends_on "python" => :recommended
+    depends_on "tokutek/tokumx/tokumx-bin"
+    depends_on "rabbitmq"
+    
     def install
         system "echo hi"
     end
